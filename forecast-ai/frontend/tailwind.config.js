cd9334a7181ef792ab1bab7c86262a98a148043e
kangwork <irene.kang@mail.utoronto.ca>
2024-10-01 19:24:42 -0400

    Add tailwind.config.js, which is a file used to configure Tailwind CSS Note that this file extends the default configuration of Tailwind CSS.

---
+++ forecast-ai/frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'light-purple': '#6B6B84',
+        'title-light-grey': '#B0B1AF',
+        'screen-black': '#191A1A',
+        'mid-dark-grey': '#2E2E2E',
+        'mid-light-grey': '#838383',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
