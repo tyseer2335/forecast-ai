2f5b20863c57f69cd0df2016159c117a90839642
YehyunLee <glad7cu@gmail.com>
2024-10-03 23:19:52 -0400

    Early work of getting news. There's a LOT of conversation right now, how many queries per promt, how many news to collect per query, how much portion of Twitter data to use.

--- forecast-ai/backend/main.py
+++ forecast-ai/backend/main.py
@@ -1,11 +1,35 @@
 from fastapi import FastAPI
-
+from gnews import GNews
 app = FastAPI()
 
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the API"}
+
+
+@app.get("/get/forecasting_news/")
+def get_forecasting_news(queries: [str], max_results: int = 10, language: str = 'en', country: str = 'US',
+                         period: str = '7d', start_date: str = None, end_date: str = None,
+                         exclude_websites: [str] or None = None,
+                         proxy: str = None):
+    """
+    Assumption:
+    User typed forecasting question.
+    Other function break down the question into multiple forecasting related queries.
+
+    This function get multiple forecasting related queries.
+    Use the queries to get news, articles, and X/Twitter posts.
+
+    By default, we return 10 results for each query.
+    User can specify the number of results to return.
+    """
+    google_news = GNews(language=language, country=country, period=period, start_date=start_date, end_date=end_date,
+                        exclude_websites=exclude_websites, proxy=proxy, max_results=max_results)
+    all_news_per_query = {}
+    for query in queries:
+        all_news_per_query[query] = google_news.get_news(query)
+    return all_news_per_query
 
 # pip install "uvicorn[standard]"
 # uvicorn main:app
