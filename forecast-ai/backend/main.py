869da6ac597cd04d36a5a2992a03af61556a78ee
YehyunLee <glad7cu@gmail.com>
2024-10-04 14:45:25 -0400

    Reorganizing file and folders. By breaking down, it's easier to manage different functions. DB CRUD folder will be used for general backend work needed for web app. Ex) getting user data.
    @Edison, I suggest moving those code in main to break down query.py.
    
    Thanks.

--- forecast-ai/backend/main.py
+++ forecast-ai/backend/main.py
@@ -1,18 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI 
 from openai import OpenAI
-from gnews import GNews
-from pydantic import BaseModel
-from typing import List, Optional
-from datetime import date
 from dotenv import load_dotenv
 import os
+from query_to_answer import break_down_query, collect_news, generate_forecast
+from query_to_answer.break_down_query import ForecastRequest
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
 
-load_dotenv()
-
+# [Initialize FastAPI app]
+# pip install "uvicorn[standard]"
+# uvicorn main:app
 app = FastAPI()
-client = OpenAI()
-
+load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAPI_API_KEY')
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 
 class ForecastRequest(BaseModel):
@@ -21,47 +23,27 @@ class ForecastRequest(BaseModel):
     start_date: Optional[date] = None
     end_date: Optional[date] = None
 
-def generate_search_queries(question: str) -> List[str]:
-    prompt = f"Break down the following forecast question into 5 key search queries: {question}"
-    response = client.chat.completions.create(
-        model="gpt-4o-mini", 
-        messages=[{ "role": "user", "content": prompt }]
-    )
-    return [query.strip() for query in response.choices[0].message.split('\n') if query.strip()]
 
-def get_forecasting_news(queries: List[str], max_results: int = 10, language: str = 'en', country: str = 'US',
-                         period: str = '7d', start_date: str = None, end_date: str = None,
-                         exclude_websites: List[str] = None,
-                         proxy: str = None):
-    """
-    Assumption:
-    User typed forecasting question.
-    Other function break down the question into multiple forecasting related queries.
+@app.post("/query_to_answer")
+def query_to_answer(request: ForecastRequest):
+    search_queries = break_down_query.generate_search_queries(request.question)
+    news = collect_news.collect_news(search_queries, max_results=request.num_articles, start_date=request.start_date, end_date=request.end_date)
+    answer = generate_forecast.generate_forecast(news)
+    return answer
 
-    This function get multiple forecasting related queries.
-    Use the queries to get news, articles, and X/Twitter posts.
+# def generate_search_queries(question: str) -> List[str]:
+#     prompt = f"Break down the following forecast question into 5 key search queries: {question}"
+#     response = client.chat.completions.create(
+#         model="gpt-4o-mini", 
+#         messages=[{ "role": "user", "content": prompt }]
+#     )
+#     return [query.strip() for query in response.choices[0].message.split('\n') if query.strip()]
 
-    By default, we return 10 results for each query.
-    User can specify the number of results to return.
-    """
-    google_news = GNews(language=language, country=country, period=period, start_date=start_date, end_date=end_date,
-                        exclude_websites=exclude_websites, proxy=proxy, max_results=max_results)
-    all_news_per_query = {}
-    for query in queries:
-        all_news_per_query[query] = google_news.get_news(query)
-    return all_news_per_query
 
-@app.get("/")
-def root():
-    return {"message": "Welcome to the API"}
-
-@app.post("/forecast")
-def forecast(request: ForecastRequest):
-    try:
-        search_queries = generate_search_queries(request.question)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error generating search queries: {str(e)}")
-    return search_queries
-
-# pip install "uvicorn[standard]"
-# uvicorn main:app
+# @app.post("/forecast")
+# def forecast(request: ForecastRequest):
+#     try:
+#         search_queries = generate_search_queries(request.question)
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Error generating search queries: {str(e)}")
+#     return search_queries
