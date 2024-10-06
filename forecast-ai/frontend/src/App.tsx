3ed6bb869ab6d95f9a964bebe7c8bbb2838ff008
unknown <tyseer2334@gmail.com>
2024-10-06 03:06:47 -0400

    Implemented Responsive Login Page

--- forecast-ai/frontend/src/App.tsx
+++ forecast-ai/frontend/src/App.tsx
@@ -16,4 +16,4 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default App; 
--- forecast-ai/frontend/src/components/Login.tsx
+++ forecast-ai/frontend/src/components/Login.tsx
@@ -4,7 +4,9 @@ import OwlLogo from '../assets/owl.svg';
 import GoogleLogo from '../assets/google-logo.svg';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, signInWithGoogle } from "./firebase";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; 
+import "../css/login-custom-css.css";
+
 
 const Login: React.FC = () => {
     const navigate = useNavigate();
@@ -42,7 +44,7 @@ const Login: React.FC = () => {
       <div className="flex flex-col items-center space-y-6 w-1/3">
 
         {/* Logo and Title */}
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 center-mobile">
           <img src={OwlLogo} alt="logo" className="w-12 h-12" />
           <h1 className="text-3xl text-title-light-grey font-light">forecastAI</h1>
         </div>
@@ -56,7 +58,7 @@ const Login: React.FC = () => {
             required
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
-            className="px-4 py-2 bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full"
+            className="text-input-mobile px-4 py-3 input-fields-mobile bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full input-mobile button-mobile"
         />
         <input
             id="password"
@@ -65,7 +67,7 @@ const Login: React.FC = () => {
             required
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
-            className="px-4 py-2 bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full"
+            className="text-input-mobile px-4 py-3 input-fields-mobile bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full input-mobile"
         />
         </div>
 
@@ -77,15 +79,17 @@ const Login: React.FC = () => {
             onClick={onGoogleLogin}
             className="flex items-center justify-center space-x-2 px-4 py-2 bg-white text-black rounded-3xl w-72"
         >
-          <img src={GoogleLogo} alt="Google Logo" className="w-5 h-5" />
+          <img src={GoogleLogo} alt="Google Logo" className="w-5 h-5 " />
           <span>Sign in with Google</span>
         </button>
 
+
+
         {/* Buttons Row */}
-        <div className="flex justify-between w-full">
-          {/* Learn More Button */}
+        <div className="flex justify-between w-full buttons-row">
+          {/* Learn More Button */}  
           <button 
-            className="px-4 py-2 font-bold text-title-light-grey bg-black rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+            className="px-4 py-2 learn-more-button-mobile font-bold text-title-light-grey bg-black rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
           >
             learn more
             </button>
@@ -93,18 +97,20 @@ const Login: React.FC = () => {
           <div className="flex space-x-2">
             <button 
                 onClick={() => navigate("/signup")}
-                className="px-4 py-2 font-bold bg-white text-light-purple border border-light-purple rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+                className=" px-4 py-2 signup-login-button-mobile font-bold bg-white text-light-purple border border-light-purple rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
             >
               Sign Up
             </button>
             <button 
                 onClick={onLogin}
-                className="px-4 py-2 font-bold text-white border border-white rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+                className=" px-4 py-2 signup-login-button-mobile font-bold text-white border border-white rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
             >
               Login
             </button>
           </div>
-        </div>
+        </div> 
+
+        
       </div>
     </div>
   );
