3495ae76d549b65172c5ad0adf5a88f4b5c41838
kangwork <irene.kang@mail.utoronto.ca>
2024-10-06 10:26:32 +0900

    Add some font colors and updated the font color namings that were used in Login and Sidebar pages

--- forecast-ai/frontend/tailwind.config.js
+++ forecast-ai/frontend/tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-purple': '#6B6B84',
-        'title-light-grey': '#B0B1AF',
+        'logo-purple': '#6B6B84',
+        'light-purple': '#AEB0FF',
+
+        'light-grey': '#B0B1AF', // ACACAC
         'screen-black': '#191A1A',
         'mid-dark-grey': '#2E2E2E',
         'mid-light-grey': '#838383',
+        // Sidebar
+        'sidebar-bg': '#202222',
+        'button-hover': '#3A3A3A',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
