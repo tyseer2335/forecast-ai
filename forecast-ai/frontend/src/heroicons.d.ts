0a426b4b6b2dd8b8002c5a640662b2a576362f53
unknown <tyseer2334@gmail.com>
2024-10-10 14:23:19 -0400

    Added Signup page, alongsidebasic logic for password validation

---
+++ forecast-ai/frontend/src/heroicons.d.ts
@@ -0,0 +1,5 @@
+declare module '@heroicons/react/24/outline' {
+    export const EyeIcon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    export const EyeSlashIcon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  }
+  
