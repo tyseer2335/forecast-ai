b4988bcc72e411c8b3feb3bf5c340578c61ed7b2
YehyunLee <glad7cu@gmail.com>
2024-10-11 23:38:32 -0400

    advanced_selenium_scrape_content use selenium for blocked platform content scrapping

--- forecast-ai/backend/query_to_answer/scrapping_content.py
+++ forecast-ai/backend/query_to_answer/scrapping_content.py
@@ -4,6 +4,9 @@
 import requests
 from bs4 import BeautifulSoup
 import html2text
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 def _single_scrape_content(url: str) -> dict:
@@ -16,11 +19,37 @@ def _single_scrape_content(url: str) -> dict:
     clean_text = cleaner.handle(str(soup))
 
     # Extract text content from common tags
-    text_elements = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ol', 'ul', 'blockquote', 'a', 'b', 'i', 'strong', 'em', 'mark', 'small', 'del', 'ins', 'sub', 'sup', 'code', 'pre', 'abbr', 'address', 'bdo', 'br', 'cite', 'dfn', 'kbd', 'q', 'samp', 'var', 'time', 'ruby', 'rt', 'rp', 'bdi', 'wbr', 'span', 'div'])
+    text_elements = soup.find_all(['p', 'h1', 'h2', 'h3', 'span', 'div'])
     clean_text = ' '.join([elem.get_text(strip=True) for elem in text_elements])
 
     # Extract media content
     media = [img['src'] for img in soup.find_all('img')]
+
+    return {
+        'text': clean_text,
+        'media': media
+    }
+
+
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(options=options)
+
+
+def advanced_selenium_scrape_content(url: str) -> dict:
+    driver.get(url)
+    # once we get redirected to the page, we need to wait for the page to load
+    # wait till news.google.com is not in the url
+    while 'news.google.com' in driver.current_url:
+        pass
+
+    # Extract text content
+    clean_text = driver.find_element(By.TAG_NAME, 'body').text
+
+    # Extract media content
+    media = [img.get_attribute('src') for img in driver.find_elements(By.TAG_NAME, 'img')]
+    # clean media: remove None and any text, only links
+    media = [link for link in media if link]
 
     return {
         'text': clean_text,
@@ -33,4 +62,12 @@ def multiple_scrape_content(urls: dict) -> dict:
     for _, news in urls.items():
         for article in news:
             article['content'] = _single_scrape_content(article['url'])
+    for _, news in urls.items():
+        for article in news:
+            if not article['content']['text']:
+                res = advanced_selenium_scrape_content(article['url'])
+                article['content']['text'] = res['text']
+                if not article['content']['media']:
+                    article['content']['media'] = res['media']
+    driver.quit()
     return urls
