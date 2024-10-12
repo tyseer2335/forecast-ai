427716fb4454af739920539dda846b6c673bfe66
YehyunLee <glad7cu@gmail.com>
2024-10-11 20:31:32 -0400

    Skip None as a default.

--- forecast-ai/backend/query_to_answer/collect_news.py
+++ forecast-ai/backend/query_to_answer/collect_news.py
@@ -1,12 +1,11 @@
 from gnews import GNews
-from typing import List
 from model.forecast_request import ForecastRequest
 from utils.process_date import convert_str_to_datetime
 
 
-def _get_forecasting_news(queries: List[str], max_results: int = 10, language: str = 'en', country: str = 'US',
+def _get_forecasting_news(queries: list[str], max_results: int = 10, language: str = 'en', country: str = 'US',
                           period: str = '7d', start_date: str = None, end_date: str = None,
-                          exclude_websites: List[str] = None,
+                          exclude_websites: list[str] = None,
                           proxy: str = None) -> dict:
     """
     Assumption:
@@ -19,14 +18,15 @@ def _get_forecasting_news(queries: List[str], max_results: int = 10, language: s
     By default, we return 10 results for each query.
     User can specify the number of results to return.
     """
-    google_news = GNews(language=language, country=country, period=period, start_date=convert_str_to_datetime(start_date), end_date=convert_str_to_datetime(end_date),
-                        exclude_websites=exclude_websites, proxy=proxy, max_results=max_results)
+    google_news = GNews(max_results=max_results, language=language, country=country, period=period,
+                        start_date=convert_str_to_datetime(start_date), end_date=convert_str_to_datetime(end_date),
+                        exclude_websites=exclude_websites, proxy=proxy)
     all_news_per_query = {}
     for query in queries:
         all_news_per_query[query] = google_news.get_news(query)
     return all_news_per_query
 
 
-def collect_news(queries: List[str], forecastRequest: ForecastRequest) -> dict:
+def collect_news(queries: list[str], forecastRequest: ForecastRequest) -> dict:
     return _get_forecasting_news(queries, max_results=forecastRequest.before_ranking_num_articles,
                                  start_date=forecastRequest.start_date, end_date=forecastRequest.end_date)
