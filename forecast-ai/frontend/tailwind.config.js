d788355a1f8584399da736160a214d98a3cf50db
kangwork <irene.kang@mail.utoronto.ca>
2024-10-12 17:42:41 +0900

    Merge and add comments to indicate the styling components

--- forecast-ai/frontend/tailwind.config.js
+++ forecast-ai/frontend/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'logo-purple': '#6B6B84',
         'light-purple': '#AEB0FF',
 
-        'light-grey': '#B0B1AF', // ACACAC
+        'light-grey': '#B0B1AF',
         'screen-black': '#191A1A',
         'mid-dark-grey': '#2E2E2E',
         'mid-light-grey': '#838383',
