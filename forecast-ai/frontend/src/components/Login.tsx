106ffece87f8ba65007a7a7cd67623cc98259391
Muaj Ahmed <muajahmed17@gmail.com>
2024-10-12 18:43:13 -0400

    Added user to DB if they sign in with google

--- forecast-ai/frontend/src/components/Login.tsx
+++ forecast-ai/frontend/src/components/Login.tsx
@@ -5,108 +5,133 @@ import GoogleLogo from '../assets/google-logo.svg';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, signInWithGoogle } from "./firebase";
 import { useNavigate } from "react-router-dom";
+import { getFirestore, doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore"; // Firestore imports
+
+const db = getFirestore(); // Initialize Firestore
 
 const Login: React.FC = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-  const onLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        navigate("/");
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
-  };
+    // Function to check if the user has a Firestore document
+    const checkAndCreateUserDocument = async (userId: string, email: string) => {
+        const userDocRef = doc(db, "Users", userId);
+        const userDoc = await getDoc(userDocRef);
 
-    const onGoogleLogin = () => {
-        signInWithGoogle()
-          .then(() => {
+        if (!userDoc.exists()) {
+            // If the user document doesn't exist, create it
+            await setDoc(userDocRef, {
+                email: email,
+                createdAt: serverTimestamp() 
+            });
+            console.log("User document created for:", email);
+        } else {
+            console.log("User document already exists for:", email);
+        }
+    };
+
+    // Login with email and password
+    const onLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            console.log("User logged in:", user);
+            // Check if a user document exists and create one if it doesn't
+            await checkAndCreateUserDocument(user.uid, user.email || "");
+            navigate("/"); // Navigate to home page after login
+        } catch (error: any) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        }
+    };
+
+    // Login with Google
+    const onGoogleLogin = async () => {
+        try {
+            const result = await signInWithGoogle(); // signInWithGoogle returns UserCredential
+            const user = result.user; // Access user from result
+            console.log("Google login successful:", user);
+            // Check if a user document exists and create one if it doesn't
+            await checkAndCreateUserDocument(user.uid, user.email || "");
             navigate("/");
-          })
-          .catch((error: any) => {
-            console.log(error);
-          });
-      };
-  return (
-    <div className="min-h-screen bg-screen-black flex items-center justify-center font-inter">
-      <div className="flex flex-col items-center space-y-6 w-1/3">
+        } catch (error: any) {
+            console.log("Google login error:", error);
+        }
+    };
 
-        {/* Logo and Title */}
-        <div className="flex items-center space-x-4">
-          <img src={OwlLogo} alt="logo" className="w-12 h-12" />
-          <h1 className="text-3xl text-light-grey font-light">forecastAI</h1>
+    return (
+        <div className="min-h-screen bg-screen-black flex items-center justify-center font-inter">
+            <div className="flex flex-col items-center space-y-6 w-1/3">
+                {/* Logo and Title */}
+                <div className="flex items-center space-x-4">
+                    <img src={OwlLogo} alt="logo" className="w-12 h-12" />
+                    <h1 className="text-3xl text-light-grey font-light">forecastAI</h1>
+                </div>
+
+                {/* Input Fields */}
+                <div className="flex flex-col space-y-4 w-full">
+                    <input
+                        id="email-address"
+                        name="email"
+                        type="email"
+                        required
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="px-4 py-2 bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full"
+                    />
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="px-4 py-2 bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full"
+                    />
+                </div>
+
+                {/* Or Text */}
+                <div className="text-xl font-light text-light-grey">or</div>
+
+                {/* Sign in with Google */}
+                <button
+                    onClick={onGoogleLogin}
+                    className="flex items-center justify-center space-x-2 px-4 py-2 bg-white text-black rounded-3xl w-72"
+                >
+                    <img src={GoogleLogo} alt="Google Logo" className="w-5 h-5" />
+                    <span>Sign in with Google</span>
+                </button>
+
+                {/* Buttons Row */}
+                <div className="flex justify-between w-full">
+                    {/* Learn More Button */}
+                    <button
+                        className="px-4 py-2 font-bold text-light-grey bg-black rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+                    >
+                        learn more
+                    </button>
+                    {/* Sign Up and Login Buttons */}
+                    <div className="flex space-x-2">
+                        <button
+                            onClick={() => navigate("/signup")}
+                            className="px-4 py-2 font-bold bg-white text-logo-purple border border-logo-purple rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+                        >
+                            Sign Up
+                        </button>
+                        <button
+                            onClick={onLogin}
+                            className="px-4 py-2 font-bold text-white border border-white rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        {/* Input Fields */}
-        <div className="flex flex-col space-y-4 w-full">
-        <input
-            id="email-address"
-            name="email"
-            type="email"
-            required
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="px-4 py-2 bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full"
-        />
-        <input
-            id="password"
-            name="password"
-            type="password"
-            required
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="px-4 py-2 bg-mid-dark-grey text-mid-light-grey rounded-2xl focus:outline-none w-full"
-        />
-        </div>
-
-        {/* Or Text */}
-        <div className="text-xl font-light text-light-grey">or</div>
-
-        {/* Sign in with Google */}
-        <button 
-            onClick={onGoogleLogin}
-            className="flex items-center justify-center space-x-2 px-4 py-2 bg-white text-black rounded-3xl w-72"
-        >
-          <img src={GoogleLogo} alt="Google Logo" className="w-5 h-5" />
-          <span>Sign in with Google</span>
-        </button>
-
-        {/* Buttons Row */}
-        <div className="flex justify-between w-full">
-          {/* Learn More Button */}
-          <button 
-            className="px-4 py-2 font-bold text-light-grey bg-black rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
-          >
-            learn more
-            </button>
-          {/* Sign Up and Login Buttons */}
-          <div className="flex space-x-2">
-            <button 
-                onClick={() => navigate("/signup")}
-                className="px-4 py-2 font-bold bg-white text-logo-purple border border-logo-purple rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
-            >
-              Sign Up
-            </button>
-            <button 
-                onClick={onLogin}
-                className="px-4 py-2 font-bold text-white border border-white rounded-3xl transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
-            >
-              Login
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
