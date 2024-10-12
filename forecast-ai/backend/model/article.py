e69dec03f441815b019bbaf9e57e7d9623c6cc3f
YehyunLee <glad7cu@gmail.com>
2024-10-12 15:26:10 -0400

    New fields added for Article class, to ensure more data can be used.

--- forecast-ai/backend/model/article.py
+++ forecast-ai/backend/model/article.py
@@ -1,12 +1,23 @@
 class Article:
-    
-    def __init__(self, title, description, fulltext, source, media):
+
+    def __init__(self, query: str, platform: str, title: str, description: str, published_date: str, url: str,
+                 publisher: dict[str, str], content: dict[str, str]):
+        query: str
+        platform: str
+        title: str
+        description: str
+        published_date: str
+        url: str
+        publisher: dict[str, str]  # 'publisher': {'href': 'https://x.com', 'title': 'X'},
+        content: dict[str, str]  # 'content': {'text': '', 'media': []}},
+
+        self.query = query
+        self.platform = platform
+
         self.title = title
         self.description = description
-        self.fulltext = fulltext
-        self.source = source
-        self.media = media
+        self.published_date = published_date
+        self.url = url
+        self.publisher = publisher
+        self.content = content
         self.score = None
-    
-    def to_json():
-        return
