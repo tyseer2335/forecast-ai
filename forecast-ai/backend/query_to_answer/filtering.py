6495b385ccf1928c808a7e884d91defa4daa0a9c
Aditya Ohri <aditya.ohri@berkeley.edu>
2024-10-13 14:02:43 -0400

    update ranking to group by source, update pipeline

--- forecast-ai/backend/query_to_answer/filtering.py
+++ forecast-ai/backend/query_to_answer/filtering.py
@@ -24,24 +24,29 @@ Your response should look like the following:
 Thoughts: {{ insert your thinking }}
 Rating: {{ insert your rating }}"""
 
-def get_relevance_score(articles, forecasting_question, client):
+def get_relevance_score(articles: dict[str, Article], forecasting_question: str, client: any):
     # get relevance score for each article wrt original forecasting question using LLM
-    for article in articles:
-        article_text = "\n---\nTitle: {title}\n\n{text}\n---\n".format(title=article.title, text=article.summary)
-        prompt = relevance_prompt.format(question=forecasting_question, article=article_text)
-        response = client.chat.completions.create(
-        model="gpt-4o-mini", 
-        messages=[{ "role": "user", "content": prompt }]
-        )
-        loc = response.choices[0].message.split("Rating:")
-        rating = loc[1].split()[0]
-        if rating.isnumeric():
-            article.score = float(rating)
-        else:
-            article.score = 1.0 # for not numeric (invalid) rating
+    for key in article.keys():
+        for article in articles[key]:
+            article_text = "\n---\nTitle: {title}\n\n{text}\n---\n".format(title=article.title, text=article.summary)
+            prompt = relevance_prompt.format(question=forecasting_question, article=article_text)
+            response = client.chat.completions.create(
+            model="gpt-4o-mini", 
+            messages=[{ "role": "user", "content": prompt }]
+            )
+            loc = response.choices[0].message.split("Rating:")
+            rating = loc[1].split()[0]
+            if rating.isnumeric():
+                article.score = float(rating)
+            else:
+                article.score = 1.0 # for not numeric (invalid) rating
 
 
-def sort_and_filter(articles, n):
+def sort_and_filter(articles: dict[str, Article], n: int, percentage_per_source: dict[str, float]) -> dict[str, Article]:
     # return N most relevant articles
-    sorted_articles = sorted(articles, key=lambda article: article.score, reverse=True)
-    return sorted_articles[:n]
+    filtered_articles = {}
+    for source in articles.keys():
+        sorted_articles = sorted(articles[source], key=lambda article: article.score, reverse=True)
+        num_articles = n * percentage_per_source[source]
+        filtered_articles[source] = sorted_articles[:num_articles]
+    return filtered_articles
