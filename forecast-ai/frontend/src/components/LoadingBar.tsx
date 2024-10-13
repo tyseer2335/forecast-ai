832d60f115303929f5116aa67227193e8d65f3e4
Edison Liem <edisonliem417@gmail.com>
2024-10-13 17:42:00 -0400

    Connect backend response to frontend & display all the news sources from the response

--- forecast-ai/frontend/src/components/LoadingBar.tsx
+++ forecast-ai/frontend/src/components/LoadingBar.tsx
@@ -4,7 +4,7 @@ import "../css/loading-bar-custom-css.css";
 
 const LoadingBar: React.FC = () => {
     return (
-        <div className="h-full flex flex-col items-center justify-center">
+        <div className="h-[604px] flex flex-col items-center justify-center">
             <div className="loader ease-linear rounded-full border-4 border-t-4 border-mid-light-grey h-12 w-12 mb-4"></div>
             <h2 className="text-center text-chat-message-text text-lg font-semibold">Loading...</h2>
             <p className="w-1/3 text-center text-chat-message-text">This may take a few seconds, please don't close this page.</p>
--- forecast-ai/frontend/src/components/SourcesContainer.tsx
+++ forecast-ai/frontend/src/components/SourcesContainer.tsx
@@ -29,14 +29,14 @@ const SourcesContainer: React.FC<SourcesContainerProps> = ({ sources, error, loa
     }
 
     return (
-        <div className="w-full flex-grow space-y-4 bg-screen-black flex flex-col h-[90%] relative">
+        <div className="w-full flex-grow space-y-4 bg-screen-black flex flex-col h-[90%]">
             {loading ? (
                 <LoadingBar />
             ) : (
                 error ? (
                     <ErrorMessage error={error} />
                 ) : (
-                    <div className="w-full flex-grow space-y-4 bg-screen-black flex flex-col h-[90%] relative">
+                    <div className="w-full flex-grow space-y-4 bg-screen-black flex flex-col h-[604px] relative">
                         <h1 className="font-bold text-chat-message-text text-xl">Sources</h1>
                         <SourceSection source={sources[currentSource]} />
                         <button onClick={decrementCurrentSource} className="absolute left-0 top-1/2 cursor-pointer">
