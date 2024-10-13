348158b67552f22c6d08ffc31a6046a23d64235f
YehyunLee <glad7cu@gmail.com>
2024-10-13 15:28:03 -0400

    Formatting

--- forecast-ai/backend/query_to_answer/filtering.py
+++ forecast-ai/backend/query_to_answer/filtering.py
@@ -19,7 +19,8 @@ Please rate the relevance of the article to the question, at the scale of 1-6
 Guidelines:
 - You don't need to access any external sources. Just consider the information provided.
 - Focus on the content of the article, not the title.
-- If the text content is an error message about JavaScript, paywall, cookies or other technical issues, output a score of 1.
+- If the text content is an error message about JavaScript, paywall, cookies or other technical issues, output a score \
+of 1.
 
 Your response should look like the following:
 Thoughts: {{ insert your thinking }}
