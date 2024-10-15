d30914edf56225a1155b6a7c33e4f9cfab8b716a
YehyunLee <glad7cu@gmail.com>
2024-10-14 21:34:44 -0400

    Added req, env test cases

--- forecast-ai/backend/unit_test/unit_test.py
+++ forecast-ai/backend/unit_test/unit_test.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 from query_to_answer import break_down_query, collect_news, scrapping_content, filtering, generate_forecast
 from utils import convert_to_article
 from model.forecast_request import ForecastRequest
@@ -10,14 +13,25 @@ def unit_test_all():
     pass
 
 
+def test_env_var() -> OpenAI:
+    load_dotenv()
+    OPENAI_API_KEY = os.getenv('OPENAPI_API_KEY')
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    LOCAL_OR_PROD = os.getenv('LOCAL_OR_PROD')  # set to `local` or `prod`. use `local` for testing selenium
+    assert isinstance(OPENAI_API_KEY, str)
+    assert isinstance(client, OpenAI)
+    assert isinstance(LOCAL_OR_PROD, str) and (LOCAL_OR_PROD == 'local' or LOCAL_OR_PROD == 'prod')
+    return client
+
+
 def test_create_request() -> ForecastRequest:
     request = ForecastRequest(question="Who will win the US 2024 election?",
-                                num_queries=5,
-                                perc_of_each_source={'automatic': 0.6, 'x.com': 0.2, 'facebook.com': 0.2},
-                                before_ranking_num_articles=10,
-                                after_ranking_num_articles=5,
-                                start_date=None,
-                                end_date=None)
+                              num_queries=5,
+                              perc_of_each_source={'automatic': 0.6, 'x.com': 0.2, 'facebook.com': 0.2},
+                              before_ranking_num_articles=10,
+                              after_ranking_num_articles=5,
+                              start_date=None,
+                              end_date=None)
     assert isinstance(request, ForecastRequest)
     assert request.question == "Who will win the US 2024 election?"
     return request
