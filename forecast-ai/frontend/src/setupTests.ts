ec3a7217f88a531942ec91e3a5cae95240b8fce9
Edison Liem <edisonliem417@gmail.com>
2024-10-16 17:23:39 -0400

    Add necessary libraries for unit testing

--- forecast-ai/frontend/src/setupTests.ts
+++ forecast-ai/frontend/src/setupTests.ts
@@ -1,2 +1,10 @@
 // src/setupTests.ts
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+import { ReadableStream } from "web-streams-polyfill";
+
+global.TextEncoder = TextEncoder;
+// @ts-expect-error
+global.TextDecoder = TextDecoder;
+// @ts-expect-error
+global.ReadableStream = ReadableStream;
