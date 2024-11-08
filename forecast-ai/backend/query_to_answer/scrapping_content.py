d8a0fa357a5c2454f84ce73100a0bae17ee7afa2
YehyunLee <glad7cu@gmail.com>
2024-11-08 12:46:53 -0500

    import multi processing

--- forecast-ai/backend/query_to_answer/scrapping_content.py
+++ forecast-ai/backend/query_to_answer/scrapping_content.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import multiprocessing
+import concurrent.futures
 
 
 def _single_scrape_content(url: str) -> dict:
