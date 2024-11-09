3d6d676c145a16dc399a99f8454a775630cea5a3
YehyunLee <glad7cu@gmail.com>
2024-11-08 20:24:46 -0500

    minutes instead of seconds msg

--- forecast-ai/frontend/src/components/LoadingBar.tsx
+++ forecast-ai/frontend/src/components/LoadingBar.tsx
@@ -11,7 +11,7 @@ const LoadingBar: React.FC<LoadingBarProps> = ({ status }) => {
         <div className="h-[604px] flex flex-col items-center justify-center">
             <div className="loader ease-linear rounded-full border-4 border-t-4 border-mid-light-grey h-12 w-12 mb-4"></div>
             <h2 className="text-center text-chat-message-text text-lg font-semibold">{status || 'Loading...'}</h2>
-            <p className="w-1/3 text-center text-chat-message-text">This may take a few seconds, please don't close this page.</p>
+            <p className="w-1/3 text-center text-chat-message-text">This may take a few minutes, please don't close this page.</p>
         </div>
     )
 }
