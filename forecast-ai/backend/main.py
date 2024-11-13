8d1a800773e55901b52be7b1eb48f1bd497b3665
YehyunLee <glad7cu@gmail.com>
2024-11-13 18:30:26 -0500

    Bias generation done by Yehyun. Took 3~6 hours

--- forecast-ai/backend/main.py
+++ forecast-ai/backend/main.py
@@ -68,65 +68,65 @@ async def query_to_answer(request: ForecastRequest, query_id: str):
     state = 0
     print("Start")
     try:
-        state = 1
-        await send_status_update(query_id, "Generating search queries...")
-        # Generate search queries
-        search_queries = break_down_query.generate_search_queries(client, request)
-        if search_queries["success"] is False:
-            raise HTTPException(status_code=500, detail=f"Error generating answer to query. State: {state}")
-
-        state = 2
-        await asyncio.sleep(0)
-        await send_status_update(query_id, "Collecting news...")
-        # Collect news
-        # return dict; key as query, value as list of dict with title, description, published date, url, publisher
-        news = collect_news.collect_news(search_queries["queries"], request)
-
-        state = 3
-        await asyncio.sleep(0)
-        await send_status_update(query_id, "Scraping content...")
-        # Content added to each news
-        # return dict; key as query, value as list of dict with title, description, published date, url, publisher,
-        # content: dict with text and media
-        # Example:
-        # {'query1': [{'title1': '...', 'description': '...', 'published date': '...', 'url': '...', 'publisher': '...',
-        # 'content': {'text': '...', 'media': ['...']}}]}
-        print(time.time())
-        news_with_content = scrapping_content.multiple_scrape_content(news, LOCAL_OR_PROD,
-                                                                      DOCKER_OR_LAMBDATEST, SINGLE_OR_PARALLEL,
-                                                                      USERNAME, ACCESS_KEY)
-        # print(news_with_content)
-        # await send_status_update(query_id, str(news_with_content))
-
-        state = 4
-        await asyncio.sleep(0)
-        await send_status_update(query_id, "Converting news objects to articles...")
-        news_objects = convert_to_article.dict_to_article(news_with_content)
-
-        state = 5
-        await asyncio.sleep(0)
-        await send_status_update(query_id, "Filtering and scoring relevance...")
-        filtering.get_relevance_score(news_objects, request.question, client)
-
-        state = 6
-        await asyncio.sleep(0)
-        await send_status_update(query_id, "Ranking news articles...")
-        ranked_news_with_content = filtering.sort_and_filter(news_objects, request.after_ranking_num_articles,
-                                                             request.perc_of_each_source)
-
-        state = 7
-        await asyncio.sleep(0)
-        await send_status_update(query_id, "Generating forecast answer...")
-        forecast_agent = generate_forecast.ForecastGenerator(client=client, model="gpt-4")
-        answer = forecast_agent.generate_forecast(request, ranked_news_with_content)["answer"]
-        # answer = {'Question': 'is LLM truely reach AGI?', 'Forecaster ID': 'AI-Forecaster', 'Forecaster Rationale': "Key Facts:\n1. As of my pretraining knowledge cutoff in October 2023, the AI technology referred to as LLM (likely referring to large language models) had not reached AGI (Artificial General Intelligence). AGI refers to highly autonomous systems that outperform humans at most economically valuable work, and as of then, no AI system had demonstrated this level of capability.\n", 'Forecast': '10.0%', 'Sources': {'x.com': [{'title': 'Alex Volkov (Thursd/AI) (@altryne) on X - X', 'content': {}, 'url': 'https://news.google.com/rss/articles/CBMioAFBVV95cUxPNHFZQW0zOUM3ZVhtZy1HVEZmbXBSdlFubi1OYWFjSktBMEdlaks0NU84UmJaSTlyMF9tZkU0dERkdURhTzN2VVd0RkFhem15Q21POEpacUllc3p2cnZpRjdMRzV4ell1WGx0enhHc29IaVdHNE9hWjFoRDczY1l6ckwzLUJ0WFp2aGx1SGJOTkpycVRReVN0YVA5c2ZDMGxT?oc=5&hl=en-CA&gl=CA&ceid=CA:en', 'published_date': 'Mon, 11 Nov 2024 21:57:00 GMT', 'platform': 'automatic'}]}}
+        # state = 1
+        # await send_status_update(query_id, "Generating search queries...")
+        # # Generate search queries
+        # search_queries = break_down_query.generate_search_queries(client, request)
+        # if search_queries["success"] is False:
+        #     raise HTTPException(status_code=500, detail=f"Error generating answer to query. State: {state}")
+        #
+        # state = 2
+        # await asyncio.sleep(0)
+        # await send_status_update(query_id, "Collecting news...")
+        # # Collect news
+        # # return dict; key as query, value as list of dict with title, description, published date, url, publisher
+        # news = collect_news.collect_news(search_queries["queries"], request)
+        #
+        # state = 3
+        # await asyncio.sleep(0)
+        # await send_status_update(query_id, "Scraping content...")
+        # # Content added to each news
+        # # return dict; key as query, value as list of dict with title, description, published date, url, publisher,
+        # # content: dict with text and media
+        # # Example:
+        # # {'query1': [{'title1': '...', 'description': '...', 'published date': '...', 'url': '...', 'publisher': '...',
+        # # 'content': {'text': '...', 'media': ['...']}}]}
+        # print(time.time())
+        # news_with_content = scrapping_content.multiple_scrape_content(news, LOCAL_OR_PROD,
+        #                                                               DOCKER_OR_LAMBDATEST, SINGLE_OR_PARALLEL,
+        #                                                               USERNAME, ACCESS_KEY)
+        # # print(news_with_content)
+        # # await send_status_update(query_id, str(news_with_content))
+        #
+        # state = 4
+        # await asyncio.sleep(0)
+        # await send_status_update(query_id, "Converting news objects to articles...")
+        # news_objects = convert_to_article.dict_to_article(news_with_content)
+        #
+        # state = 5
+        # await asyncio.sleep(0)
+        # await send_status_update(query_id, "Filtering and scoring relevance...")
+        # filtering.get_relevance_score(news_objects, request.question, client)
+        #
+        # state = 6
+        # await asyncio.sleep(0)
+        # await send_status_update(query_id, "Ranking news articles...")
+        # ranked_news_with_content = filtering.sort_and_filter(news_objects, request.after_ranking_num_articles,
+        #                                                      request.perc_of_each_source)
+        #
+        # state = 7
+        # await asyncio.sleep(0)
+        # await send_status_update(query_id, "Generating forecast answer...")
+        # forecast_agent = generate_forecast.ForecastGenerator(client=client, model="gpt-4")
+        # answer = forecast_agent.generate_forecast(request, ranked_news_with_content)["answer"]
+        answer = {'Question': 'is LLM truely reach AGI?', 'Forecaster ID': 'AI-Forecaster', 'Forecaster Rationale': "Key Facts:\n1. As of my pretraining knowledge cutoff in October 2023, the AI technology referred to as LLM (likely referring to large language models) had not reached AGI (Artificial General Intelligence). AGI refers to highly autonomous systems that outperform humans at most economically valuable work, and as of then, no AI system had demonstrated this level of capability.\n", 'Forecast': '10.0%', 'Sources': {'x.com': [{'title': 'Alex Volkov (Thursd/AI) (@altryne) on X - X', 'content': {}, 'url': 'https://news.google.com/rss/articles/CBMioAFBVV95cUxPNHFZQW0zOUM3ZVhtZy1HVEZmbXBSdlFubi1OYWFjSktBMEdlaks0NU84UmJaSTlyMF9tZkU0dERkdURhTzN2VVd0RkFhem15Q21POEpacUllc3p2cnZpRjdMRzV4ell1WGx0enhHc29IaVdHNE9hWjFoRDczY1l6ckwzLUJ0WFp2aGx1SGJOTkpycVRReVN0YVA5c2ZDMGxT?oc=5&hl=en-CA&gl=CA&ceid=CA:en', 'published_date': 'Mon, 11 Nov 2024 21:57:00 GMT', 'platform': 'automatic'}]}}
         print(answer)
 
         state = 8
         await asyncio.sleep(0)
         await send_status_update(query_id, "Generating bias...")
         answer_with_bias = generate_bias.generate_bias(answer, client)
-        # {'Question': 'is LLM truely reach AGI?', 'Forecaster ID': 'AI-Forecaster', 'Forecaster Rationale': 'Key Facts:\n1. As of my pretraining knowledge cutoff in October 2023, the AI technology referred to as LLM (likely referring to
+        # {'Question': 'is LLM enough to reach AGI?', 'Forecaster ID': 'AI-Forecaster', 'Forecaster Rationale': 'Key Facts:\n1. As of my pretraining knowledge cutoff in October 2023, the AI technology referred to as LLM (likely referring to
         # large language models) had not reached AGI (Artificial General Intelligence). AGI refers to highly autonomous systems that outperform humans at most economically valuable work, and as of then, no AI system had demonstrated this
         # level of capability.\n', 'Forecast': '10.0%', 'Sources': {'x.com': [{'title': 'Alex Volkov (Thursd/AI) (@altryne) on X - X', 'content': {}, 'url': 'https://news.google.com/rss/articles/CBMioAFBVV95cUxPNHFZQW0zOUM3ZVhtZy1HVEZmbXB
         # SdlFubi1OYWFjSktBMEdlaks0NU84UmJaSTlyMF9tZkU0dERkdURhTzN2VVd0RkFhem15Q21POEpacUllc3p2cnZpRjdMRzV4ell1WGx0enhHc29IaVdHNE9hWjFoRDczY1l6ckwzLUJ0WFp2aGx1SGJOTkpycVRReVN0YVA5c2ZDMGxT?oc=5&hl=en-CA&gl=CA&ceid=CA:en', 'published_date':
